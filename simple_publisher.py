29cf5cfbc8c485a0f226ba5a2c07b9f8f13d6b65
Elissa <eito@mit.edu>
2023-03-03 21:22:14 -0500

    Question 1: Simple Publisher

---
+++ simple_publisher.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+import random
+from std_msgs.msg import Float32
+
+def simple_publisher():
+    pub = rospy.Publisher('my_random_float', Float32)
+    rospy.init_node('simple_publisher', anonymous = True)
+    rate = rospy.Rate(20) #20hz
+    while not rospy.is_shutdown():
+        random_float = random.uniform(0,10)
+        rospy.loginfo(random_float)
+        pub.publish(random_float)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        simple_publisher()
+    except rospy.ROSInterruptException:
+        pass
